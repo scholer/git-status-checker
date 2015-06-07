ecb87770c356c57baeb81a816c8cb57be205ed63
Rasmus Scholer <rasmusscholer@gmail.com>
2015-06-07 16:53:42 -0400

    Adding batch launcher.

---
+++ bin/git_status_checker.bat
@@ -0,0 +1,9 @@
+@echo off
+
+REM Change to projects root directory:
+cd %~dp0\..\..
+
+REM Invoke git_status_checker script from projects root directory:
+python %~dp0\git_status_checker.py --ignore-untracked %* .
+
+IF ERRORLEVEL 1 pause
